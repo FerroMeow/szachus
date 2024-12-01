a90a2f8d262d38ab01c1ce98f8f27a32c253fbf6
Konrad Balcerzak <kobdi4@gmail.com>
2024-12-01 22:22:53 +0100

    Change active turn on the beginning of the game

--- src/network/systems.rs
+++ src/network/systems.rs
@@ -1,19 +1,26 @@
 use bevy::prelude::{NextState, Res, ResMut};
 
-use crate::game::{resources::PlayerColorResource, GameState};
+use crate::game::{resources::PlayerColorResource, GameState, TurnState};
 
-use super::{resources::WebsocketChannels, GameWsUpdateMsg, MatchmakingResponse};
+use super::{
+    resources::WebsocketChannels, ChessPieceColorEnum, GameWsUpdateMsg, MatchmakingResponse,
+};
 
 pub(crate) fn ws_get_color(
-    websocket_channels: Res<WebsocketChannels>,
     mut next_game_state: ResMut<NextState<GameState>>,
     mut player_color: ResMut<PlayerColorResource>,
+    mut turn_state: ResMut<NextState<TurnState>>,
+    websocket_channels: Res<WebsocketChannels>,
 ) {
     let Ok(GameWsUpdateMsg::Matchmaking(MatchmakingResponse::Success { color })) =
         websocket_channels.rx_updates.try_recv()
     else {
         return;
     };
+    turn_state.set(match color {
+        ChessPieceColorEnum::White => TurnState::PlayersTurn,
+        ChessPieceColorEnum::Black => TurnState::WaitingTurn,
+    });
     player_color.0 = color;
     next_game_state.set(GameState::Playing);
 }
